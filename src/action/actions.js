46b9e51a0130df122cc03e7189bb9b8f05f2f5a6
Hoang Van Duc <duch52362@gmail.com>
2024-08-28 08:38:37 +0700

    Fetch Data Use Redux

--- src/action/actions.js
+++ src/action/actions.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { INCREMENT, DECREMENT } from "./types";
 
 export const increaseCounter = () => {
@@ -9,5 +10,39 @@ export const increaseCounter = () => {
 export const decreaseCounter = () => {
   return {
     type: DECREMENT,
+  };
+};
+
+// start doing finish
+export const fetchAllUsers = () => {
+  return async (dispatch, getState) => {
+    dispatch(fetchUsersRequest());
+    try {
+      const res = await axios.get("http://localhost:8080/users/all");
+      const data = res && res.data ? res.data : [];
+      dispatch(fetchUsersSuccess(data));
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchUsersError());
+    }
+  };
+};
+
+export const fetchUsersRequest = () => {
+  return {
+    type: "FETCH_USER_REQUEST",
+  };
+};
+
+export const fetchUsersSuccess = (data) => {
+  return {
+    type: "FETCH_USER_SUCCESS",
+    dataUsers: data,
+  };
+};
+
+export const fetchUsersError = () => {
+  return {
+    type: "FETCH_USER_ERROR",
   };
 };
--- src/component/TableUser.js
+++ src/component/TableUser.js
@@ -3,17 +3,24 @@ import { Container } from "react-bootstrap";
 import Table from "react-bootstrap/Table";
 import axios from "axios";
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAllUsers } from "../action/actions";
 
 function TableUser(props) {
-  const [listUser, setListUser] = useState([]);
-  const fetchAllUser = async () => {
-    const res = await axios.get("http://localhost:8080/users/all");
-    const data = res && res.data ? res.data : [];
-    setListUser(data);
-  };
+  // const [, setListUser] = useState([]);
+
+  // const fetchAllUser = async () => {
+  //   const res = await axios.get("http://localhost:8080/users/all");
+  //   const data = res && res.data ? res.data : [];
+  //   setListUser(data);
+  // };
+  const dispatch = useDispatch();
+  const listUser = useSelector((state) => state.user.listUsers);
+  console.log("listUsers: ", listUser);
 
   useEffect(() => {
-    fetchAllUser();
+    // fetchAllUser();
+    dispatch(fetchAllUsers());
   }, []);
 
   const handleDeleteUser = (item) => {
